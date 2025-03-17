4f757a8b53a29c6d3b7a0015f4df3f38a06651f9
kingsleyeffiong <kingsleyeffiong642@gmail.com>
2025-03-17 08:18:21 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
             <Route path="/sign-up" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="/" element={<Dashboard />}>
               <Route index element={<UI />} />
               <Route path="/dashboard" element={<UI />} />
               <Route path="notification" element={<Notification />} />
